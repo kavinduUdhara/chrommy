6317e20f21b185ceccedd2df4c88c01423a30906
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-11-24 19:24:52 +0530

    enable side panel on action click and update tab options for google.com

--- dict/background.js
+++ dict/background.js
@@ -1,12 +1,19 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason === 'install') {
-    chrome.storage.local.set({
-      apiSuggestions: ['tabs', 'storage', 'scripting']
-    });
-  }
-})
 
-import './popUp/popUpSummary.js';
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on google.com
+  await chrome.sidePanel.setOptions({
+    tabId,
+    path: "sidePannel/index.html",
+    enabled: true,
+  });
+});
