c96e20e6339514b0dc55e0c7c18a0da812c2262b
kavinduUdhara <wmkavinduudhara@gmail.com>
2024-11-26 03:40:30 +0530

    added some files and make app.jsx with help of old slide panel

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./sidePanel.css";
+
+import { TbSend2 } from "react-icons/tb";
+import { PiArrowDownRightBold } from "react-icons/pi";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-holder">
+        <div className="greeting-holder">
+          <img src="./img/light-bulb.png" className="img-light" />
+          <img src="./img/chair.png" className="img-chair" />
+          <div className="text">
+            <h1 className="greeting">Good Evening!</h1>
+            <p>Welcome to chrommy</p>
+          </div>
+        </div>
+        <div className="page-info-holder">
+          <div className="page-info">
+            <img
+              id="fav-icon"
+              src="../assets/icon-32.png"
+              alt="Favicon"
+              className="fav-icon"
+            />
+            <div className="more-info">
+              <p id="tab-title">Chrommy</p>
+              <p id="tab-url">github.com</p>
+            </div>
+          </div>
+          <button className="ac-btn">
+            Summarize
+            <PiArrowDownRightBold />
+          </button>
+        </div>
+        <div className="chat-box-send-holder">
+          <div className="chat-box-send">
+            <textarea
+              id="chat-input"
+              rows="1"
+              placeholder="Type a message..."
+            ></textarea>
+            <button id="send-btn">
+            <TbSend2 />
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite/vite.config.js
+++ vite/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dict/hi',
+    outDir: '../dict/sidePanel',
   },
-  base: '/hi/',
+  base: '/sidePanel/',
 });
